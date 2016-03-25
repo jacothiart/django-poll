4522b0532b1347f6920faee829a87f9e9bb9eaea
Jaco Thiart <jaco.thiart.android@gmail.com>
2016-03-25 14:22:58 +0200

    django poll ppackage manager build

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='django-poll',
+      version='0.3.1',
+      description='Django Poll',
+      url='https://github.com/jacothiart/django-poll',
+      author='Jaco Thiart',
+      author_email='jaco@jacothiart.com',
+      license='MIT',
+      packages=['poll'],
+      zip_safe=False)
